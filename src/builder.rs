916b9a52fe05b755da63340f1e48495c00ca9426
akthrmsx <akt.hrms@gmail.com>
2025-09-26 23:37:12 +0900

    commit

--- src/builder.rs
+++ src/builder.rs
@@ -1,7 +1,49 @@
 use proc_macro2::TokenStream;
-use quote::quote;
-use syn::DeriveInput;
+use quote::{format_ident, quote};
+use syn::{Data, DeriveInput, Fields, FieldsNamed};
 
-pub fn implement(_: DeriveInput) -> TokenStream {
-    quote! {}
+pub fn implement(input: DeriveInput) -> TokenStream {
+    let product_name = input.ident;
+    let visibility = input.vis;
+    let builder_name = format_ident!("{}Builder", product_name);
+    let fields_named = extract_fields_named(&input.data);
+
+    let builder_fields = fields_named.named.iter().map(|field| {
+        let identifier = field.ident.clone().unwrap();
+        let ty = field.ty.clone();
+        quote! {
+            #identifier: Option<#ty>
+        }
+    });
+
+    let initial_values = fields_named.named.iter().map(|field| {
+        let identifier = field.ident.clone().unwrap();
+        quote! {
+            #identifier: None
+        }
+    });
+
+    quote! {
+        #visibility struct #builder_name {
+            #(#builder_fields),*
+        }
+
+        impl #product_name {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    #(#initial_values),*
+                }
+            }
+        }
+    }
+}
+
+fn extract_fields_named(data: &Data) -> FieldsNamed {
+    match data {
+        Data::Struct(data) => match &data.fields {
+            Fields::Named(fields) => fields.clone(),
+            _ => panic!("expects named fields"),
+        },
+        _ => panic!("expects struct"),
+    }
 }
