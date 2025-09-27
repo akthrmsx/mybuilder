d36dc8a0077735274ffcc68e8e2dc06f6b146923
akthrmsx <akt.hrms@gmail.com>
2025-09-27 15:22:05 +0900

    commit

--- src/builder.rs
+++ src/builder.rs
@@ -1,31 +1,48 @@
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
-use syn::{Data, DeriveInput, Fields, FieldsNamed};
+use syn::{
+    Data, DeriveInput, Fields, FieldsNamed, GenericArgument, PathArguments, PathSegment, Type,
+};
 
 pub fn implement(input: DeriveInput) -> TokenStream {
     let product_name = input.ident;
     let visibility = input.vis;
     let builder_name = format_ident!("{}Builder", product_name);
-    let fields_named = extract_fields_named(&input.data);
+    let fields = extract_named_fields(&input.data);
 
-    let builder_fields = fields_named.named.iter().map(|field| {
+    let builder_fields = fields.named.iter().map(|field| {
         let identifier = field.ident.clone().unwrap();
         let ty = field.ty.clone();
-        quote! {
-            #identifier: Option<#ty>
+
+        if is_option(&ty) {
+            quote! {
+                #identifier: #ty
+            }
+        } else {
+            quote! {
+                #identifier: Option<#ty>
+            }
         }
     });
 
-    let initial_values = fields_named.named.iter().map(|field| {
+    let initial_values = fields.named.iter().map(|field| {
         let identifier = field.ident.clone().unwrap();
         quote! {
             #identifier: None
         }
     });
 
-    let set_values = fields_named.named.iter().map(|field| {
+    let set_values = fields.named.iter().map(|field| {
         let identifier = field.ident.clone().unwrap();
-        let ty = field.ty.clone();
+        let ty = {
+            let ty = &field.ty;
+
+            if is_option(ty) {
+                unwrap_option(ty).clone()
+            } else {
+                ty.clone()
+            }
+        };
         quote! {
             #visibility fn #identifier(&mut self, #identifier: #ty) -> &mut Self {
                 self.#identifier = Some(#identifier);
@@ -34,20 +51,32 @@ pub fn implement(input: DeriveInput) -> TokenStream {
         }
     });
 
-    let validate_values = fields_named.named.iter().map(|field| {
-        let identifier = field.ident.clone().unwrap();
-        let err = format!("required field '{}' is missing", identifier);
-        quote! {
-            if self.#identifier.is_none() {
-                return Err(#err.into());
+    let validate_values = fields
+        .named
+        .iter()
+        .filter(|field| !is_option(&field.ty))
+        .map(|field| {
+            let identifier = field.ident.clone().unwrap();
+            let err = format!("required field '{}' is missing", identifier);
+            quote! {
+                if self.#identifier.is_none() {
+                    return Err(#err.into());
+                }
             }
-        }
-    });
+        });
 
-    let build_values = fields_named.named.iter().map(|field| {
+    let build_values = fields.named.iter().map(|field| {
         let identifier = field.ident.clone().unwrap();
-        quote! {
-            #identifier: self.#identifier.clone().unwrap()
+        let ty = field.ty.clone();
+
+        if is_option(&ty) {
+            quote! {
+                #identifier: self.#identifier.clone()
+            }
+        } else {
+            quote! {
+                #identifier: self.#identifier.clone().unwrap()
+            }
         }
     });
 
@@ -78,12 +107,45 @@ pub fn implement(input: DeriveInput) -> TokenStream {
     }
 }
 
-fn extract_fields_named(data: &Data) -> FieldsNamed {
+fn extract_named_fields(data: &Data) -> &FieldsNamed {
     match data {
         Data::Struct(data) => match &data.fields {
-            Fields::Named(fields) => fields.clone(),
+            Fields::Named(fields) => fields,
             _ => panic!("expects named fields"),
         },
         _ => panic!("expects struct"),
+    }
+}
+
+fn extract_last_path_segment(ty: &Type) -> Option<&PathSegment> {
+    match ty {
+        Type::Path(ty) => ty.path.segments.last(),
+        _ => None,
+    }
+}
+
+fn extract_first_generic_argument(segment: &PathSegment) -> Option<&GenericArgument> {
+    match &segment.arguments {
+        PathArguments::AngleBracketed(arguments) => arguments.args.first(),
+        _ => None,
+    }
+}
+
+fn is_option(ty: &Type) -> bool {
+    match extract_last_path_segment(ty) {
+        Some(segment) => segment.ident == "Option",
+        _ => false,
+    }
+}
+
+fn unwrap_option(ty: &Type) -> &Type {
+    match extract_last_path_segment(ty) {
+        Some(segment) if segment.ident == "Option" => {
+            match extract_first_generic_argument(segment) {
+                Some(GenericArgument::Type(ty)) => ty,
+                _ => panic!("expects generic argument type"),
+            }
+        }
+        _ => panic!("expects option"),
     }
 }
